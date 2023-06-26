ec717b72ffc1b6600985727599f5dffbca3fb6d7
brucemug <mugzbru199@gmail.com>
2023-06-26 23:41:14 +0300

    edited app file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Link manager app')
+})
+
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+app.listen(port, console.log(`Server is listening on port ${port}...`))
