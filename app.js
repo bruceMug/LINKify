feadf597d63036a897696eeee90643d3ebcdacfa
brucemug <mugzbru199@gmail.com>
2023-07-10 21:20:53 +0300

    added middleware

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const tasks = require("./routes/cards")
+
+//middleware
+app.use(express.json())
+
 
 // routes
 app.get('/hello', (req, res) => {
     res.send('Link manager app')
 })
+
+app.use('/api/v1/tasks', tasks)
+
+
+// app.post('/api/v1/login)             -logging in
+//  app.get('/api/v1/tasks)             -get all tasks
+// app.get('api/v1/tasks/:id')          -get single task
+// app.post('/api/v1/tasks')            -create new task
+// app.delete('/api/v1/tasks/:id)       -delete card
 
 const port = 3000;
 
